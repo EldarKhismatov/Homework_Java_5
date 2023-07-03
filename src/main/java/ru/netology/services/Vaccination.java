6686ac15650428a6c09dcba902de8ca50a61d0c2
Eldar <ekhismatov@list.ru>
2023-07-03 17:16:13 +0300

    2 commit

--- src/main/java/ru/netology/services/Vaccination.java
+++ src/main/java/ru/netology/services/Vaccination.java
@@ -1,17 +1,19 @@
 package ru.netology.services;
 public class Vaccination {
     public int calculate(int income, int expenses, int threshold) {
-            int count = 0; // счётчик месяцев отдыха
-            int money = 0; // количество денег на счету
-            for (int month = 0; month < 12; month++) {
-                if (money >= threshold) { // можем ли отдыхать?
+        income = 10_000;
+        expenses = 3_000; // обязательные месячные траты
+        threshold = 20_000; // есть возможность отдохнуть
+        int count = 0; // счётчик месяцев отдыха
+        int money = 0; // количество денег на счету
+        for (int month = 0; month < 12; month++) {
+            if (money >= threshold) { // можем ли отдыхать?
                     count++; // увеличиваем счётчик месяцев отдыха
                     money = (money - expenses) / 3;
-                } else {
+            } else {
                     money = money + income - expenses;
-                }
             }
-            return count;
+        }return count;
     }
 }
 
--- src/test/java/VaccanationTest.java
+++ src/test/java/VaccanationTest.java
@@ -5,12 +5,9 @@ import ru.netology.services.Vaccination;
 
 public class VaccanationTest {
     @Test
-     void testCashVacancion(int expected, int income, int expenses, int threshold) {
+     void testCashVacancion(int income, int expenses, int threshold) {
         Vaccination vaccination = new Vaccination();
-        expected = 3;
-        income = 10_000;
-        expenses = 3_000; // обязательные месячные траты
-        threshold = 20_000; // есть возможность отдохнуть
+        int expected = 3;
         int actual = vaccination.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
